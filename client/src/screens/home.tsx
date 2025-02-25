8a2d076c20f8ff142062f3f7a6019f02c1ece9e0
Nelson <nelsonfeng.18@gmail.com>
2025-02-24 20:49:27 -0500

    Remove copied events.tsx, fix type errors

--- client/src/screens/home.tsx
+++ client/src/screens/home.tsx
@@ -1,8 +1,8 @@
-import { Redirect } from 'expo-router';
+import { Redirect, type RedirectProps } from 'expo-router';
 
 export default function Home() {
   // Assuming user is authenticated
   // You can add a condition here to check auth status
 
-  return <Redirect href="(tabs)" />;
+  return <Redirect href={"(tabs)" as RedirectProps["href"]} />;
 }
